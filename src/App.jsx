f49d4dd5273e15a009ce253ed7bb243889f506bc
Ram Tadong <r.tadong@dubai.nextcalibur.com>
2024-01-05 17:20:18 +0400

    m

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1 className="mx-3 border-8 border-solid border-red-600 p-3 px-2 text-center text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
